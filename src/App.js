c6642ece9366bf24818a86e30385d53c8abeaed1
rupesagr <rupesh.galaxy@gmail.com>
2023-10-26 12:37:24 +0530

    added client

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [data, setData] = useState(null);
+
+  const getAppData = async () => {
+    console.log("getAppData called");
+    const response = await fetch("http://localhost:3001/api");
+    console.log("response=" + response);
+    const resJSON = await response.json();
+    console.log("message=" + resJSON.message);
+    setData(resJSON.message);
+  };
+
+  useEffect(() => {
+    getAppData();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>{!data ? "Loading..." : data}</p>
       </header>
     </div>
   );
